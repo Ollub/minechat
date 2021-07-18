30121244e057055799c142ccb195430d91f78e0b
Oleg Orlov <orlovoa@pik.ru>
2021-07-18 10:12:24 +0300

    Rewrite on client class

--- minechat/main.py
+++ minechat/main.py
@@ -10,40 +10,67 @@ def write_log(msg: str) -> None:
         log_file.write(msg)
 
 
-async def listen_chat():
-    reader, writer = await asyncio.open_connection(settings.HOST, settings.PORT_OUT)
+class Client:
+    """Minechat client."""
 
-    while True:
-        data = await reader.readline()
+    _reader: asyncio.StreamReader
+    _writer: asyncio.StreamWriter
+
+    def __init__(self):
+        asyncio.ensure_future(self.connect())
+
+    async def connect(self):
+        self._reader = await self.get_reader()
+        self._writer = await self.get_writer()
+
+    async def get_reader(self) -> asyncio.StreamReader:
+        reader, writer = await asyncio.open_connection(settings.HOST, settings.PORT_OUT)
+        return reader
+
+    async def get_writer(self) -> asyncio.StreamWriter:
+        reader, writer = await asyncio.open_connection(settings.HOST, settings.PORT_IN)
+        return writer
+
+    async def authenticate(self):
+        msg = settings.TOKEN + "\n"
+        await self._send_msg(msg)
+        print("Authenticated")
+
+    async def consume(self):
+        while True:
+            msg = await self._fetch_msg()
+            print(msg)
+            write_log(msg)
+
+    async def produce(self):
+        while True:
+            msg = input("Enter your message: ")
+            if msg == "exit":
+                break
+            msg += "\n\n"
+            await self._send_msg(msg)
+
+    async def close(self):
+        print("Close the connection")
+        self._writer.close()
+
+    async def _fetch_msg(self) -> str:
+        data = await self._reader.readline()
         timestamp_str = dt.datetime.now().strftime(settings.DATETIME_FMT)
         msg = f"[{timestamp_str}] {data.decode()}"
-        print(msg)
-        write_log(msg)
+        return msg
 
-    print("Close the connection")
-    writer.close()
-
-
-async def send_to_chat():
-    reader, writer = await asyncio.open_connection(settings.HOST, settings.PORT_IN)
-
-    # authentication
-    writer.write((settings.TOKEN + "\n").encode())
-
-    while True:
-        msg = input("Enter your message: ")
-        if msg == "exit":
-            break
-        msg += "\n\n"
-        writer.write(msg.encode())
-
-    print("Close the connection")
-    writer.close()
+    async def _send_msg(self, msg: str) -> None:
+        self._writer.write(msg.encode())
 
 
 async def main():
-    # await listen_chat()
-    await send_to_chat()
+    cli = Client()
+    # TODO: replace sleep with smth
+    await asyncio.sleep(1)  # wait for connection
+    await cli.authenticate()
+    await cli.produce()
+    await cli.close()
 
 
 if __name__ == "__main__":
